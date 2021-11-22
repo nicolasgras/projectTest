46038a5ae07fdc594c41a3a9c56c7525572991d3
NicolasGras <nicogras@hotmail.com>
2021-11-22 10:02:09 -0500

    Update App.java

--- consumerBanking/src/main/java/com/companyname/bank/App.java
+++ consumerBanking/src/main/java/com/companyname/bank/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         String test = null;
-        test.split('/');
+        test.split("/");
         System.out.println( "Hello World!" );
     }
 }
